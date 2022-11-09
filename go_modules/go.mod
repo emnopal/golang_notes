c8f5a793c97f455ccafe89981d0079eedd4a660b
Muhammad Naufal <mnaufal0999@gmail.com>
2022-11-09 09:17:00 +0700

    changes

--- go_modules/go.mod
+++ go_modules/go.mod
@@ -2,4 +2,4 @@ module sample // do not attemp this in real case, use git instead
 
 go 1.19
 
-require github.com/emnopal/private-go-module v1.0.1
+require github.com/emnopal/private-go-module/v2 v2.0.0
--- go_modules/main.go
+++ go_modules/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
-	private_go_module "github.com/emnopal/private-go-module"
+	private_go_module "github.com/emnopal/private-go-module/v2"
 )
 
 func main() {
-	result := private_go_module.TestPrivateGoModule()
+	result := private_go_module.TestPrivateGoModule("Hello")
 	fmt.Println(result)
 	private_go_module.PrintTestGoModule()
 }
